debea954640606e35662b8393bc6cdc48f62f241
jpramassini <jpramassini@gmail.com>
2019-10-01 22:05:35 -0400

    Added visitor pattern to Expr and GenerateAst files. Added class to ugly-print syntax trees.

--- src/com/craftinginterpreters/tool/GenerateAst.java
+++ src/com/craftinginterpreters/tool/GenerateAst.java
@@ -34,20 +34,41 @@ public class GenerateAst {
         writer.println("import java.util.List;");
         writer.println();
         writer.println("abstract class " + baseName + " {");
-            for (String type : types){
-                String className = type.split(":")[0].trim();
-                String fields = type.split(":")[1].trim();
-                defineType(writer, baseName, className, fields);
-            }
+
+        defineVisitor(writer, baseName, types);
+
+            // define AST classes
+        for (String type : types){
+            String className = type.split(":")[0].trim();
+            String fields = type.split(":")[1].trim();
+            defineType(writer, baseName, className, fields);
+        }
+
+        // base accept() method
+        writer.println("    abstract <R> R accept(Visitor<R> visitor);");
+
         writer.println("}");
         writer.println();
         writer.close();
     }
 
+    private static void defineVisitor(
+            PrintWriter writer, String baseName, List<String> types) {
+        writer.println("    interface Visitor<R> {");
+
+        for (String type : types) {
+            String typeName = type.split(":")[0].trim();
+            writer.println("        R visit" + typeName + baseName + "(" +
+                typeName + " " + baseName.toLowerCase() + ");");
+        }
+
+        writer.println("    }");
+    }
+
     private static void defineType(
             PrintWriter writer, String baseName,
             String className, String fieldList) {
-        writer.println("  static class " + className + " extends " +
+        writer.println("    static class " + className + " extends " +
             baseName + " {");
 
         // Constructor
@@ -57,19 +78,27 @@ public class GenerateAst {
         String[] fields = fieldList.split(", ");
         for (String field : fields) {
             String name = field.split(" ")[1];
-            writer.println("    this." + name + " = " + name + ";");
+            writer.println("            this." + name + " = " + name + ";");
         }
 
         // Close out that constructor
-        writer.println("    }");
+        writer.println("        }");
+
+        // Visitor pattern
+        writer.println();
+        writer.println("        <R> R accept(Visitor<R> visitor) {");
+        writer.println("            return visitor.visit" +
+            className + baseName + "(this);");
+        writer.println("        }");
 
         // Put those fields in the class
         writer.println();
         for (String field : fields){
-            writer.println("    final " + field + ";");
+            writer.println("        final " + field + ";");
         }
 
         // Close the class.
-        writer.println(" }");
+        writer.println("    }");
+        writer.println();
     }
 }
